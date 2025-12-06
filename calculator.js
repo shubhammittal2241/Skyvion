2ffc4265eb44685a98bbc963112063145bfc2441
Shubham <shubhammittal2241@gmail.com>
2025-12-06 11:51:07 +0530

    added calculator

---
+++ calculator.js
@@ -0,0 +1,36 @@
+let loanrange = document.getElementById("amountRange");
+let loaninput = document.getElementById("amountInput");
+loanrange.addEventListener("input", function () {
+  loaninput.value = loanrange.value;
+});
+let raterange = document.getElementById("rateRange");
+let rateinput = document.getElementById("rateInput");
+raterange.addEventListener("input", function () {
+  rateinput.value = raterange.value;
+});
+let timerange = document.getElementById("tenureRange");
+let timeinput = document.getElementById("tenureInput");
+timerange.addEventListener("input", function () {
+  timeinput.value = timerange.value;
+});
+
+loanrange.addEventListener("input", calculate_emi);
+raterange.addEventListener("input", calculate_emi);
+timerange.addEventListener("input", calculate_emi);
+
+document.addEventListener("DOMContentLoaded", calculate_emi);
+
+function calculate_emi() {
+  let p = Number(loaninput.value);
+  let r = Number(rateinput.value) / 12 / 100;
+  let t = Number(timeinput.value) * 12;
+
+  let emi = (p * r * Math.pow(1 + r, t)) / (Math.pow(1 + r, t) - 1);
+  let month_emi = document.getElementById("monthlyemi");
+  let total_interest = document.getElementById("totalInterest");
+  let total_payment = document.getElementById("totalAmount");
+
+  total_interest.innerText = "₹ " + (emi * t - p).toFixed(2);
+  total_payment.innerText = "₹ " + (emi * t).toFixed(2);
+  month_emi.innerText = emi.toFixed(2);
+}
---
+++ sale.js
@@ -0,0 +1,6 @@
+let house=document.getElementsByClassName("house");
+ 
+ for(i of house){
+     i.addEventListener("click",function(){
+ window.open("calculator.html", "_blank");
+     })}
